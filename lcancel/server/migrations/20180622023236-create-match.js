a6d6db2fb3ca1a17f93ed5fb6e6b46b8117d3691
David Wright <hockeyguy411@gmail.com>
2018-07-10 19:59:45 -0400

    Migrations now have FK constraints!

--- lcancel/server/migrations/20180622023236-create-match.js
+++ lcancel/server/migrations/20180622023236-create-match.js
@@ -9,19 +9,44 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       winning_user: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'users',
+          key : 'id',
+          as : 'winning_user',
+        }
       },
       losing_user: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'users',
+          key : 'id',
+          as : 'losing_user',
+        }
       },
       winning_character: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'characters',
+          key : 'id',
+          as : 'winning_character',
+        }
       },
       losing_character: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'characters',
+          key : 'id',
+          as : 'losing_character',
+        }
       },
       stage: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'stages',
+          key : 'id',
+          as : 'stage',
+        }
       },
       createdAt: {
         allowNull: false,
